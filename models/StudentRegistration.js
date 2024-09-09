4fac4babdba8df9ba418e916b5a9ec7cf1011292
merwil vegas <com2pa@gmail.com>
2024-09-08 21:32:57 -0400

    se añadio modelo para inscripcion de hijo / registro de estudiante

---
+++ models/StudentRegistration.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const StudentRegistrationSchema =new mongoose.Schema({
+    name: String, 
+    lastname:String,
+    age: Number,
+    sex:[{
+        type: String,
+        enum:['niño','niña']
+    }],
+    degree:[{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Degree'
+    }],
+    user:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }
+    
+})
+
+StudentRegistrationSchema.set('toJSON',{
+    transform: (document, returnedObject)=>{
+        returnedObject.id = returnedObject._id.toString();
+        delete returnedObject._id;
+        delete returnedObject.__v;
+    }
+})
+
+const StudentRegistration = mongoose.model('StudentRegistration',StudentRegistrationSchema);
+
+module.exports = StudentRegistration;
