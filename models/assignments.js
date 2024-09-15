530579511f2e2194255e94e2196616fd48c7b376
merwil vegas <com2pa@gmail.com>
2024-09-15 18:53:36 -0400

    modelo de asignaciones

---
+++ models/assignments.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose');
+
+const AssignmentsSchema = new  mongoose.Schema({
+    name: String,
+    tipo:[{
+        type: String,
+        enum:[
+            'individual',
+            'grupal',
+            'exposicion',
+            'examen', 
+            'trabajoEscrito',
+            'dibujo',
+            'maqueta',
+
+        ]
+    }],
+    lapso:[{
+        type:String,
+        enum:[
+            '1er lapso',
+            '2do lapso',
+            '3er lapso',
+            'Recuperacion Vacional',
+        ]
+    }],
+    subject:[{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Subject'
+    }],
+    degree:[{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Degree'
+    }],
+    teacher:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+
+    }
+})
+AssignmentsSchema.set('toJSON',{
+    transform: (document, returnedObject)=>{
+        returnedObject.id = returnedObject._id.toString();
+        delete returnedObject._id;
+        delete returnedObject.__v;
+    }
+});
+
+const Assignments = mongoose.model('Assignments', AssignmentsSchema);
+
+module.exports = Assignments;
