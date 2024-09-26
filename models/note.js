8d02c5f907cf79a44074ea1546e579b1c934ccef
merwil vegas <com2pa@gmail.com>
2024-09-26 09:56:45 -0400

    se creo modelo de para las nota de los alumnos

---
+++ models/note.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const NoteSchema =new mongoose.Schema({
+    ponderacion : Number,
+  
+    students:[{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'StudentRegistration'
+    }],
+    subject:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Subject'
+    },
+    assignment:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Assignments'
+    }
+})
+
+NoteSchema.set('toJSON',{
+    transform: (document, returnedObject)=>{
+        returnedObject.id = returnedObject._id.toString();
+        delete returnedObject._id;
+        delete returnedObject.__v;
+    }
+})
+
+const Note = mongoose.model('Note', NoteSchema);
+
+module.exports = Note;
